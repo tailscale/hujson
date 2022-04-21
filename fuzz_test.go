6583d061006469bb0e01dc6b76790cbd1d8d7614
Joe Tsai <joetsai@digital-static.net>
2022-04-21 10:03:26 -0700

    Add GitHub actions integration test (#14)

--- fuzz_test.go
+++ fuzz_test.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//+build dev.fuzz
+//go:build dev.fuzz
+// +build dev.fuzz
 
 package hujson
 
